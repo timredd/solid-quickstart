ad3a2544b06e04ad9d7ff4a49ab81ddcb18cce18
timredd <timredd13@gmail.com>
2024-12-15 11:19:42 -0500

    Add session and user api

---
+++ src/api/auth/session.ts
@@ -0,0 +1,84 @@
+import { db } from "@/db/client";
+import {
+  type Session,
+  type User,
+  sessionsTable,
+  usersTable,
+} from "@/db/schemas/auth";
+import { sha256 } from "@oslojs/crypto/sha2";
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { eq } from "drizzle-orm";
+
+export type UserSession =
+  | { session: Session; user: User }
+  | { session: null; user: null };
+
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
+
+export async function createSession(
+  token: string,
+  userId: string,
+): Promise<Session> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+  const session = await db
+    .insert(sessionsTable)
+    .values({
+      id: sessionId,
+      userId,
+      token,
+      expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    })
+    .returning()
+    .then((rows) => rows.at(0));
+  if (!session) {
+    throw new Error("Failed to create session");
+  }
+  return session;
+}
+
+export async function validateSessionToken(
+  token: string,
+): Promise<UserSession> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+  const result = await db
+    .select({ user: usersTable, session: sessionsTable })
+    .from(sessionsTable)
+    .innerJoin(usersTable, eq(sessionsTable.userId, usersTable.id))
+    .where(eq(sessionsTable.id, sessionId))
+    .then((rows) => rows.at(0));
+  if (!result) {
+    return { session: null, user: null };
+  }
+  const { user, session } = result;
+
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await db.delete(sessionsTable).where(eq(sessionsTable.id, session.id));
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await db
+      .update(sessionsTable)
+      .set({ expiresAt: session.expiresAt })
+      .where(eq(sessionsTable.id, session.id));
+  }
+  return { session, user };
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+  await db.delete(sessionsTable).where(eq(sessionsTable.id, sessionId));
+}
+
+export async function invalidateUserSessions(userId: string): Promise<void> {
+  await db.delete(sessionsTable).where(eq(sessionsTable.userId, userId));
+}
---
+++ src/api/auth/user.ts
@@ -0,0 +1,128 @@
+import { db } from "@/db/client";
+import {
+  type InsertUser,
+  InsertUserSchema,
+  type User,
+  accountsTable,
+  sessionsTable,
+  usersTable,
+} from "@/db/schemas/auth";
+import type { TxOrDb } from "@/db/transact";
+import { type Pagination, PaginationSchema } from "@/shared/types";
+import { and, asc, desc, eq, like } from "drizzle-orm";
+import type { SQLiteColumn } from "drizzle-orm/sqlite-core";
+import * as v from "valibot";
+
+export async function createUser(user: InsertUser, tx?: TxOrDb) {
+  const parsed = v.parse(InsertUserSchema, user);
+
+  const createdUser = await (tx || db)
+    .insert(usersTable)
+    .values(parsed)
+    .returning()
+    .then((rows) => rows.at(0));
+  if (!createdUser) {
+    throw new Error("Failed to create user");
+  }
+
+  return createdUser;
+}
+
+export async function getUserById(id: string): Promise<User> {
+  const user = await db.query.users.findFirst({
+    where: (t, { eq }) => eq(t.id, id),
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user;
+}
+
+export async function getUserByEmail(email: string): Promise<User> {
+  const user = await db.query.users.findFirst({
+    where: (t, { eq }) => eq(t.email, email.toLowerCase()),
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user;
+}
+
+const UpdateUserSchema = v.partial(InsertUserSchema);
+
+export async function updateUser(
+  id: string,
+  data: v.InferOutput<typeof UpdateUserSchema>,
+) {
+  const parsedData = v.parse(UpdateUserSchema, data);
+
+  const updatedUser = await db
+    .update(usersTable)
+    .set(parsedData)
+    .where(eq(usersTable.id, id))
+    .returning()
+    .then((rows) => rows.at(0));
+  if (!updatedUser) {
+    throw new Error("Failed to update user");
+  }
+
+  return updatedUser;
+}
+
+export async function deleteUser(id: string) {
+  await db.transaction(async (tx) => {
+    // Delete user's sessions
+    await tx.delete(sessionsTable).where(eq(sessionsTable.userId, id));
+    // Delete user's accounts
+    await tx.delete(accountsTable).where(eq(accountsTable.userId, id));
+    // Delete user
+    await tx.delete(usersTable).where(eq(usersTable.id, id));
+  });
+}
+
+export async function listUsers(pagination?: Pagination) {
+  const { limit, order, page, search, sort } = v.parse(
+    PaginationSchema,
+    pagination,
+  );
+
+  function getSortField(sort: "id" | "created" | "updated"): SQLiteColumn {
+    switch (sort) {
+      case "created":
+        return usersTable.createdAt;
+      case "updated":
+        return usersTable.updatedAt;
+      default:
+        return usersTable.id;
+    }
+  }
+
+  const users = await db
+    .select()
+    .from(usersTable)
+    .where(and(search ? like(usersTable.name, `%${search}%`) : undefined))
+    .orderBy((order === "asc" ? asc : desc)(getSortField(sort)))
+    .limit(limit)
+    .offset((page - 1) * limit);
+  if (!users) {
+    throw new Error("Failed to list users");
+  }
+
+  return users;
+}
+
+export async function verifyEmail(id: string) {
+  const updatedUser = await db
+    .update(usersTable)
+    .set({ emailVerified: true })
+    .where(eq(usersTable.id, id))
+    .returning()
+    .then((rows) => rows.at(0));
+  if (!updatedUser) {
+    throw new Error("Failed to verify email");
+  }
+
+  return updatedUser;
+}
