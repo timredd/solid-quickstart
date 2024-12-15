30798dd27641f2b86a9b3a934333a457c12972a9
timredd <timredd13@gmail.com>
2024-12-15 11:20:21 -0500

    Update drizzle client config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,8 @@
-import { db } from "@/db/client";
+import { adapter } from "@/db/adapter";
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "sqlite",
-    usePlural: true,
-  }),
+  database: adapter,
 });
 
 export type Session = typeof auth.$Infer.Session;
