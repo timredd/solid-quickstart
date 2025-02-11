1d91a98262130e5d58eb0a2861dc6a3ebfd38829
timredd <timredd13@gmail.com>
2025-02-11 09:13:40 -0500

    Update nanoid

--- src/lib/nanoid.ts
+++ src/lib/nanoid.ts
@@ -1,5 +1,16 @@
 import { customAlphabet } from "nanoid";
 
-export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
+const DEFAULT_SIZE = 21;
+const DEFAULT_ALPHABET = "0123456789abcdefghjkmnpqrstvwxyz";
+export const nanoid = customAlphabet(DEFAULT_ALPHABET, DEFAULT_SIZE);
+
+const LONG_SIZE = 30;
+const LONG_ALPHABET = "0123456789abcdefghjkmnpqrstvwxyz";
+export const nanoidLong = customAlphabet(LONG_ALPHABET, LONG_SIZE);
+
+const TOKEN_SIZE = 32;
+const TOKEN_ALPHABET =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+export const nanoidToken = customAlphabet(TOKEN_ALPHABET, TOKEN_SIZE);
 
 export type Nanoid = ReturnType<typeof nanoid>;
